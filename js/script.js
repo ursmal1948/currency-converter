fba2dc22610e501b190e76ef4ed0c6848a7a3f15
Urszula Malinowska <ula.malin35@gmail.com>
2022-12-21 00:53:06 +0100

    removal of unnecessary lines

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 {
-
     const calculateResult = (amount, currency) => {
 
         const rateNOK = 2.19;
@@ -10,49 +9,33 @@
         switch (currency) {
             case "NOK":
                 return amount * rateNOK;
-
             case "USD":
                 return amount * rateUSD;
-
             case "EUR":
                 return amount * rateEUR;
-
             case "GBP":
                 return amount * rateGBP;
-
         }
     };
-
     const updateResultText = (amount, result, currency) => {
-
         const resultElement = document.querySelector(".js-result");
         resultElement.innerText = `${amount} PLN = ${result.toFixed(2)} ${currency}`;
-
     }
-
     const onFormSubmit = (event) => {
         event.preventDefault();
-
         const currencyElement = document.querySelector(".js-currency");
         const amountElement = document.querySelector(".js-amount");
-
         const amount = +amountElement.value;
         const currency = currencyElement.value;
-
         const result = calculateResult(amount, currency);
-
         updateResultText(amount, result, currency);
-
     };
-
     const resetButtonElement = document.querySelector(".js-resetButton");
 
     const onButtonResetElementClick = () => {
         const resultElement = document.querySelector(".js-result");
-
         resultElement.innerText = ``;
     };
-
     resetButtonElement.addEventListener("click", onButtonResetElementClick);
 
     const init = () => {
@@ -60,5 +43,4 @@
         formElement.addEventListener("submit", onFormSubmit);
     };
     init();
-
 };
